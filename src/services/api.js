a45916581415f46c99fe8f06c73f637462e53640
MarcelinoGarcia-NA <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2022-01-29 20:35:48 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-   baseURL: 'https://novamusic.herokuapp.com/',
+   baseURL: 'https://api-novamusic.herokuapp.com/',
 });
 
 api.interceptors.request.use(async config => {
