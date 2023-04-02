459835e6dd2fe8067fc759a304fc46f2804661a5
Marcelino Garcia <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2023-04-02 01:52:11 -0300

    Update index.js

--- src/pages/PerfisUsuarios/index.js
+++ src/pages/PerfisUsuarios/index.js
@@ -17,7 +17,7 @@ export default function Categorias() {
   const [x, setX] = useState([]);
   const url_atual = window.location.href;
   const id = url_atual.substring(33);
-  const id_search_style = url_atual.substring(33);
+  const id_search_style = url_atual.substring(43);
   const search_category = url_atual.substring(33,40);
   const [deleteUser, setDeleteUser]=useState(false);
   const [icon,setIcon]=useState();
